ad40c9b5b49e8f43754756b54f5a729cbab639fd
computershawn <shontek@gmail.com>
2019-09-11 10:50:13 -0700

    Changes!

--- linkedlist.js
+++ linkedlist.js
@@ -80,6 +80,29 @@ class LinkedList {
     }
   }
 
+  find(item) {
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // Check for the item 
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+         and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking 
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+
 
 
 
